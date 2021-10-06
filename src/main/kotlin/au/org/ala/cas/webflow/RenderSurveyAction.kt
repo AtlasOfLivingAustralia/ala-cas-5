160fb078f10ecea7f07d87d11d823db0ad294477
Simon Bear <simon.bear@csiro.au>
2021-10-06 15:33:24 +1100

    Updates for survey #37  - Make survey answer mandatory  - Update options

--- src/main/kotlin/au/org/ala/cas/webflow/RenderSurveyAction.kt
+++ src/main/kotlin/au/org/ala/cas/webflow/RenderSurveyAction.kt
@@ -15,22 +15,25 @@ class RenderSurveyAction(private val messageSource: MessageSource) : AbstractAct
             mapOf(
                 "" to messageSource.getMessage("ala.affiliations.noneSelected", args, "-- Please select one --", locale),
                 "uniResearch" to messageSource.getMessage("ala.affiliations.uniResearch", args, "University – faculty, researcher", locale),
+                "uniGeneral" to messageSource.getMessage("ala.affiliations.uniGeneral", args, "University - general staff, administration, management", locale),
                 "uniStudent" to messageSource.getMessage("ala.affiliations.uniStudent", args, "University – student", locale),
-                "uniGeneral" to messageSource.getMessage("ala.affiliations.uniGeneral", args, "University - general staff, library, administration, management", locale),
-                "publiclyFunded" to messageSource.getMessage("ala.affiliations.publiclyFunded", args, "Publicly Funded Research Agency (PFRA)", locale),
+                "education" to messageSource.getMessage("ala.affiliations.education", args, "Education – primary and secondary schools, TAFE, environmental or wildlife education", locale),
+                "firstNationsOrg" to messageSource.getMessage("ala.affiliations.firstNationsOrg", args, "First Nations organisation", locale),
+                "government" to messageSource.getMessage("ala.affiliations.government", args, "Government – federal, state and local", locale),
+                "industry" to messageSource.getMessage("ala.affiliations.industry", args, "Industry, commercial, business or retail", locale),
+                "museum" to messageSource.getMessage("ala.affiliations.museum", args, "Museum, herbarium, library, botanic gardens", locale),
+                "wildlife" to messageSource.getMessage("ala.affiliations.wildlife", args, "Wildlife park, sanctuary, zoo, aquarium, wildlife rescue", locale),
+                "publiclyFunded" to messageSource.getMessage("ala.affiliations.publiclyFunded", args, "Publicly Funded Research Agency (PFRA) includes CSIRO, ANSTO, AIMS, AAO, AIATSIS, AAD, DSTO, GA, BoM", locale),
                 "mri" to messageSource.getMessage("ala.affiliations.mri", args, "Medical Research Institute (MRI)", locale),
-                "museum" to messageSource.getMessage("ala.affiliations.museum", args, "Museum, herbarium, library or other collecting organisation", locale),
-                "otherResearch" to messageSource.getMessage("ala.affiliations.otherResearch", args, "Other research organisation", locale),
-                "industry" to messageSource.getMessage("ala.affiliations.industry", args, "Industry / commercial organisation", locale),
-                "govLocal" to messageSource.getMessage("ala.affiliations.govLocal", args, "Government - local", locale),
-                "govState" to messageSource.getMessage("ala.affiliations.govState", args, "Government - state", locale),
-                "govFederal" to messageSource.getMessage("ala.affiliations.govFederal", args, "Government - federal", locale),
-                "volunteer" to messageSource.getMessage("ala.affiliations.volunteer", args, "Volunteer including citizen scientists", locale),
-                "education" to messageSource.getMessage("ala.affiliations.education", args, "Education – primary and secondary", locale),
+                "otherResearch" to messageSource.getMessage("ala.affiliations.otherResearch", args, "Other research organisation, unaffiliated researcher", locale),
                 "nfp" to messageSource.getMessage("ala.affiliations.nfp", args, "Not for profit", locale),
-                "other" to messageSource.getMessage("ala.affiliations.other", args, "Other / unaffiliated", locale),
+                "community" to messageSource.getMessage("ala.affiliations.community", args, "Community based organisation – club, society, landcare", locale),
+                "volunteer" to messageSource.getMessage("ala.affiliations.volunteer", args, "Volunteer, citizen scientist", locale),
+                "private" to messageSource.getMessage("ala.affiliations.private", args, "Private user", locale),
+                "other" to messageSource.getMessage("ala.affiliations.other", args, "Other", locale),
                 "disinclinedToAcquiesce" to messageSource.getMessage("ala.affiliations.disinclinedToAcquiesce", args, "Prefer not to say", locale)
             )
+
         )
         return success()
     }
--- src/main/kotlin/au/org/ala/cas/webflow/Survey.kt
+++ src/main/kotlin/au/org/ala/cas/webflow/Survey.kt
@@ -1,7 +1,51 @@
 package au.org.ala.cas.webflow
 
+import au.org.ala.utils.logger
+import org.hibernate.validator.constraints.NotBlank
+import org.springframework.binding.message.MessageBuilder
+import org.springframework.binding.validation.ValidationContext
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.servlet.support.RequestContext
+import org.thymeleaf.spring4.naming.SpringContextVariableNames
+import org.springframework.webflow.execution.RequestContextHolder as WFRCH
 import java.io.Serializable
+import javax.servlet.ServletRequest
+import javax.servlet.http.HttpServletRequest
 
 data class Survey @JvmOverloads constructor(
+    @NotBlank
     var affiliation: String = ""
-): Serializable
+): Serializable {
+
+    companion object {
+        val log = logger()
+    }
+
+    /**
+     * Spring Webflow validator, called dynamically by WebFlow - note this doesn't work with the Thymeleaf Spring MVC
+     * validation integration.
+     *
+     * Must use thymeleaf constructs similar to
+     * th:if="${#arrays.isEmpty(flowRequestContext.messageContext.getMessagesBySource('affiliation'))}"
+     * to detect existence of validation errors on webflow return due to validation exception.
+     *
+     * (WebMVC uses errors object on the Spring WebMVC RequestContext, this RequestContext does not exist
+     *  at this point in the webflow, possibly created for thymeleaf?)
+     */
+    fun validateUserAffiliationSurveyFormView(context: ValidationContext) {
+        log.debug("validating survey {}", this)
+        val messages = context.messageContext
+        if (affiliation.isBlank()) {
+            log.debug("Invalid affiliation")
+            messages.addMessage(
+                MessageBuilder()
+                    .error()
+                    .code("ala.screen.survey.affiliation.required")
+                    .source("affiliation")
+                    .defaultText("Affiliation is required")
+                    .build()
+            )
+        }
+    }
+
+}
