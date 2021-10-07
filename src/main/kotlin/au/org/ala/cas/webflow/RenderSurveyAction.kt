992bec61d955b6c7a91be51e3a01a22bb394291b
Simon Bear <simon.bear@csiro.au>
2021-10-07 15:01:19 +1100

    Update order of survey items to be alphabetical #37

--- src/main/kotlin/au/org/ala/cas/webflow/RenderSurveyAction.kt
+++ src/main/kotlin/au/org/ala/cas/webflow/RenderSurveyAction.kt
@@ -12,26 +12,26 @@ class RenderSurveyAction(private val messageSource: MessageSource) : AbstractAct
         val args = emptyArray<String>()
         val locale = context.externalContext.locale
         context.viewScope.put("affiliations",
-            mapOf(
+            linkedMapOf(
                 "" to messageSource.getMessage("ala.affiliations.noneSelected", args, "-- Please select one --", locale),
-                "uniResearch" to messageSource.getMessage("ala.affiliations.uniResearch", args, "University – faculty, researcher", locale),
-                "uniGeneral" to messageSource.getMessage("ala.affiliations.uniGeneral", args, "University - general staff, administration, management", locale),
-                "uniStudent" to messageSource.getMessage("ala.affiliations.uniStudent", args, "University – student", locale),
+                "community" to messageSource.getMessage("ala.affiliations.community", args, "Community based organisation – club, society, landcare", locale),
                 "education" to messageSource.getMessage("ala.affiliations.education", args, "Education – primary and secondary schools, TAFE, environmental or wildlife education", locale),
                 "firstNationsOrg" to messageSource.getMessage("ala.affiliations.firstNationsOrg", args, "First Nations organisation", locale),
                 "government" to messageSource.getMessage("ala.affiliations.government", args, "Government – federal, state and local", locale),
                 "industry" to messageSource.getMessage("ala.affiliations.industry", args, "Industry, commercial, business or retail", locale),
-                "museum" to messageSource.getMessage("ala.affiliations.museum", args, "Museum, herbarium, library, botanic gardens", locale),
-                "wildlife" to messageSource.getMessage("ala.affiliations.wildlife", args, "Wildlife park, sanctuary, zoo, aquarium, wildlife rescue", locale),
-                "publiclyFunded" to messageSource.getMessage("ala.affiliations.publiclyFunded", args, "Publicly Funded Research Agency (PFRA) includes CSIRO, ANSTO, AIMS, AAO, AIATSIS, AAD, DSTO, GA, BoM", locale),
                 "mri" to messageSource.getMessage("ala.affiliations.mri", args, "Medical Research Institute (MRI)", locale),
-                "otherResearch" to messageSource.getMessage("ala.affiliations.otherResearch", args, "Other research organisation, unaffiliated researcher", locale),
+                "museum" to messageSource.getMessage("ala.affiliations.museum", args, "Museum, herbarium, library, botanic gardens", locale),
                 "nfp" to messageSource.getMessage("ala.affiliations.nfp", args, "Not for profit", locale),
-                "community" to messageSource.getMessage("ala.affiliations.community", args, "Community based organisation – club, society, landcare", locale),
-                "volunteer" to messageSource.getMessage("ala.affiliations.volunteer", args, "Volunteer, citizen scientist", locale),
-                "private" to messageSource.getMessage("ala.affiliations.private", args, "Private user", locale),
                 "other" to messageSource.getMessage("ala.affiliations.other", args, "Other", locale),
-                "disinclinedToAcquiesce" to messageSource.getMessage("ala.affiliations.disinclinedToAcquiesce", args, "Prefer not to say", locale)
+                "otherResearch" to messageSource.getMessage("ala.affiliations.otherResearch", args, "Other research organisation, unaffiliated researcher", locale),
+                "disinclinedToAcquiesce" to messageSource.getMessage("ala.affiliations.disinclinedToAcquiesce", args, "Prefer not to say", locale),
+                "private" to messageSource.getMessage("ala.affiliations.private", args, "Private user", locale),
+                "publiclyFunded" to messageSource.getMessage("ala.affiliations.publiclyFunded", args, "Publicly Funded Research Agency (PFRA) e.g. CSIRO, AIMS, DSTO", locale),
+                "uniResearch" to messageSource.getMessage("ala.affiliations.uniResearch", args, "University – faculty, researcher", locale),
+                "uniGeneral" to messageSource.getMessage("ala.affiliations.uniGeneral", args, "University - general staff, administration, management", locale),
+                "uniStudent" to messageSource.getMessage("ala.affiliations.uniStudent", args, "University – student", locale),
+                "volunteer" to messageSource.getMessage("ala.affiliations.volunteer", args, "Volunteer, citizen scientist", locale),
+                "wildlife" to messageSource.getMessage("ala.affiliations.wildlife", args, "Wildlife park, sanctuary, zoo, aquarium, wildlife rescue", locale)
             )
 
         )
